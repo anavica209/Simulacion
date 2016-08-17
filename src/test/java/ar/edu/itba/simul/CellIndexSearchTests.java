79c757ac8fd93492a81396e1b524adb8ce1f77b9
Federico Bond <federicobond@gmail.com>
2016-08-17 16:46:56 -0300

    Adjust parameters. Test fail

--- src/test/java/ar/edu/itba/simul/CellIndexSearchTests.java
+++ src/test/java/ar/edu/itba/simul/CellIndexSearchTests.java
@@ -10,15 +10,18 @@ import static org.junit.Assert.assertEquals;
 
 public class CellIndexSearchTests {
 
+    private static final double SIZE = 100.0;
+    private static final double MAX_RADIUS = 10.0;
+
     private List<Particle> generateParticles(int num) {
         List<Particle> particles = new ArrayList<>(num);
 
         Random rand = new Random();
 
         for (int i = 0; i < num; i++) {
-            double x = rand.nextDouble();
-            double y = rand.nextDouble();
-            double radius = rand.nextDouble();
+            double x = rand.nextDouble() * SIZE;
+            double y = rand.nextDouble() * SIZE;
+            double radius = rand.nextDouble() * MAX_RADIUS;
             particles.add(new Particle(x, y, radius));
         }
 
@@ -28,8 +31,8 @@ public class CellIndexSearchTests {
     @Test
     public void equivalentToBruteSearch() {
         List<Particle> particles = generateParticles(10);
-        NeighborSearch callIndex = new CellIndexSearch(particles, 100.0, 10);
-        NeighborSearch bruteForce = new BruteForceSearch(particles, 100.0, 10);
+        NeighborSearch callIndex = new CellIndexSearch(particles, SIZE, 10);
+        NeighborSearch bruteForce = new BruteForceSearch(particles, SIZE, 10);
 
         Neighbors neighbors = callIndex.timedSearch(10.0);
         Neighbors expected = bruteForce.timedSearch(10.0);
